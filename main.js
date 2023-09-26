5d13c7965120be6bafbed560cbc3abf57383c458
danisagallardo <danysgallardo@gmail.com>
2023-09-25 22:31:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+let curso = prompt("Qué curso vas a realizar?");
+
+if (curso != null){
+    document.write("Bienvenido/a a mi sitio web, " + curso);
+} else{
+    alert ("No has introducido nada.Recarga la pagina para intentar nuevamente.");
+
+}
